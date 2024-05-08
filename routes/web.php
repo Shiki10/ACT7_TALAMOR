520935ced5950c0cc5c1c0b288e4d30991d370bb
Nikkitigue <nikkitigue029@gmail.com>
2024-05-07 17:40:17 -0700

    act7_talamor

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\ProductController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
